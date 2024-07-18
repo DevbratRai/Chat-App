108c3ea2434a0a5a42ace085f44f06d403b5ffb0
Devbrat Rai <91175819+DevbratRai@users.noreply.github.com>
2024-07-18 11:38:22 +0530

    added the token and set to the cookies also add the protectRoute middleware for the validation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import User from "../user.model.js";
+import User from "../models/user.model.js";
 import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -10,7 +12,10 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 // app.get("/", (req, res) => {
 //   res.send("hello world");
